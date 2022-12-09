2bec32c3eac1f92bbfc7f8bdeb81b4675f2d4dd4
Yoo Seungmin <dnjfht@naver.com>
2022-12-09 22:07:39 +0900

    edit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -13,21 +14,22 @@ function Home() {
     setMovies(json.data.movies);
     setLoading(false);
   };
-
   useEffect(() => {
     getMovies();
   }, []);
-
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
               id={movie.id}
+              year={movie.year}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               summary={movie.summary}
@@ -39,5 +41,4 @@ function Home() {
     </div>
   );
 }
-
 export default Home;
